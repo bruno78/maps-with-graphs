8b8fb004a26305ae90f8bb9f9a6e8601a04892fd
bruno78 <devbruno78@gmail.com>
2017-11-23 13:10:26 -0500

    Add description to readme and my previous solution for getDistance2 at GraphAdjList

--- README.md
+++ README.md
@@ -6,4 +6,10 @@ This project explore graphs using Google Maps API
 
 ### Week 2
 
-1. Implemented **Degree Sequence** which is an ordered list containing the degrees of each of the vertices in a graph, in non-increasing sorted order (with repetitions). The degree sequence of a graph is an invariant of the graph and can be used to distinguish between two graphs and to analyze graph properties.
+**Method Implementations**
+
+1. **Degree Sequence** which is an ordered list containing the degrees of each of the vertices in a graph, in non-increasing sorted order (with repetitions). The degree sequence of a graph is an invariant of the graph and can be used to distinguish between two graphs and to analyze graph properties.
+
+2. **getDistance2** (get Distance 2) finds all vertices reachable by two hops from vertex.
+  - **GraphAdjList** getDistance2 - get vertex's neighbors and loop through them adding to the new array.
+  - **GraphAdjMatrix** getDistance2 - in this case can be obtained by squaring the matrix representation of the graph. Then creating a nested loop where you loop through the column of the inner squared matrix adding all values that are greater than 0 to the new array. 
--- src/basicgraph/GraphAdjList.java
+++ src/basicgraph/GraphAdjList.java
@@ -6,13 +6,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-/** A class that implements a directed graph. 
- * The graph may have self-loops, parallel edges. 
+/** A class that implements a directed graph.
+ * The graph may have self-loops, parallel edges.
  * Vertices are labeled by integers 0 .. n-1
  * and may also have String labels.
  * The edges of the graph are not labeled.
  * Representation of edges via adjacency lists.
- * 
+ *
  * @author UCSD MOOC development team and Bruno G. Tavares
  *
  */
@@ -20,61 +20,61 @@ public class GraphAdjList extends Graph {
 
 
 	private Map<Integer,ArrayList<Integer>> adjListsMap;
-	
-	/** 
+
+	/**
 	 * Create a new empty Graph
 	 */
 	public GraphAdjList () {
 		adjListsMap = new HashMap<Integer,ArrayList<Integer>>();
 	}
 
-	/** 
-	 * Implement the abstract method for adding a vertex. 
+	/**
+	 * Implement the abstract method for adding a vertex.
 	 */
 	public void implementAddVertex() {
 		int v = getNumVertices();
 		ArrayList<Integer> neighbors = new ArrayList<Integer>();
 		adjListsMap.put(v,  neighbors);
 	}
-	
-	/** 
+
+	/**
 	 * Implement the abstract method for adding an edge.
 	 * @param v the index of the start point for the edge.
-	 * @param w the index of the end point for the edge.  
+	 * @param w the index of the end point for the edge.
 	 */
 	public void implementAddEdge(int v, int w) {
 		(adjListsMap.get(v)).add(w);
 
 	}
-	
-	/** 
-	 * Implement the abstract method for finding all 
+
+	/**
+	 * Implement the abstract method for finding all
 	 * out-neighbors of a vertex.
 	 * If there are multiple edges between the vertex
 	 * and one of its out-neighbors, this neighbor
 	 * appears once in the list for each of these edges.
-	 * 
+	 *
 	 * @param v the index of vertex.
-	 * @return List<Integer> a list of indices of vertices.  
-	 */	
+	 * @return List<Integer> a list of indices of vertices.
+	 */
 	public List<Integer> getNeighbors(int v) {
 		return new ArrayList<Integer>(adjListsMap.get(v));
 	}
 
-	/** 
-	 * Implement the abstract method for finding all 
+	/**
+	 * Implement the abstract method for finding all
 	 * in-neighbors of a vertex.
 	 * If there are multiple edges from another vertex
 	 * to this one, the neighbor
 	 * appears once in the list for each of these edges.
-	 * 
+	 *
 	 * @param v the index of vertex.
-	 * @return List<Integer> a list of indices of vertices.  
-	 */	
+	 * @return List<Integer> a list of indices of vertices.
+	 */
 	public List<Integer> getInNeighbors(int v) {
 		List<Integer> inNeighbors = new ArrayList<Integer>();
 		for (int u : adjListsMap.keySet()) {
-			//iterate through all edges in u's adjacency list and 
+			//iterate through all edges in u's adjacency list and
 			//add u to the inNeighbor list of v whenever an edge
 			//with startpoint u has endpoint v.
 			for (int w : adjListsMap.get(u)) {
@@ -85,33 +85,35 @@ public class GraphAdjList extends Graph {
 		}
 		return inNeighbors;
 	}
-	 
 
-	/** 
-	 * Implement the abstract method for finding all 
+
+	/**
+	 * Implement the abstract method for finding all
 	 * vertices reachable by two hops from v.
-	 * 
+	 *
 	 * @param v the index of vertex.
-	 * @return List<Integer> a list of indices of vertices.  
-	 */		
+	 * @return List<Integer> a list of indices of vertices.
+	 */
 	 public List<Integer> getDistance2(int v) {
 		 // XXX: Implement this method in week 2
 		 List<Integer> twoHop = new ArrayList<Integer>();
 		 List<Integer> neighbors = getNeighbors(v);
-		 /*
-		 for (int i = 0; i < neighbors.size(); i++) {	 
+		 
+		 // My solution
+		 for (int i = 0; i < neighbors.size(); i++) {
 			 int nv = neighbors.get(i);
-			 if ((getNeighbors(nv).contains(v))) {
-				 twoHop.add(nv);
+			 if (nv > 0) {
+				 twoHop.addAll(getNeighbors(nv));
 			 }
 		 }
-		 */
-		 for( int u : getNeighbors(v)){
-			 twoHop.addAll(getNeighbors(u));
-		 }
+		 
+//		 // A factored solution
+//		 for( int u : getNeighbors(v)){
+//			 twoHop.addAll(getNeighbors(u));
+//		 }
 		 return twoHop;
 	}
-	
+
 	/**
 	 * Generate string representation of adjacency list
 	 * @return the String
